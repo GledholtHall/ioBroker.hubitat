4682cfc820c25164da915034ae0a5a05d30e7693
ioBroker Adapter Creator <noreply@iobroker.net>
2021-08-06 01:58:19 +0100

    Initial commit by adapter creator

---
+++ admin/words.js
@@ -0,0 +1,53 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"hubitat adapter settings": {
+		"en": "Adapter settings for hubitat",
+		"de": "Adaptereinstellungen für hubitat",
+		"ru": "Настройки адаптера для hubitat",
+		"pt": "Configurações do adaptador para hubitat",
+		"nl": "Adapterinstellingen voor hubitat",
+		"fr": "Paramètres d'adaptateur pour hubitat",
+		"it": "Impostazioni dell'adattatore per hubitat",
+		"es": "Ajustes del adaptador para hubitat",
+		"pl": "Ustawienia adaptera dla hubitat",
+		"zh-cn": "hubitat的适配器设置"
+	},
+	"hub": {
+		"en": "Hubitat Elevation IP",
+		"de": "Hubitat Elevation IP",
+		"ru": "Hubitat Elevation IP",
+		"pt": "IP de elevação Hubitat",
+		"nl": "Hubitat Elevation IP",
+		"fr": "IP d'élévation de l'Hubitat",
+		"it": "Hubitat elevazione IP",
+		"es": "IP de elevación de Hubitat",
+		"pl": "Elewacja Hubitatu IP",
+		"zh-cn": "Hubitat 海拔 IP"
+	},
+	"port": {
+		"en": "Maker API port",
+		"de": "Maker-API-Port",
+		"ru": "Порт API Maker",
+		"pt": "Porta da API Maker",
+		"nl": "Maker API-poort",
+		"fr": "Port API Maker",
+		"it": "Porta API Maker",
+		"es": "Puerto de la API de Maker",
+		"pl": "Port API kreatora",
+		"zh-cn": "创客API端口"
+	},
+	"token": {
+		"en": "Maker API token",
+		"de": "Maker-API-Token",
+		"ru": "Токен Maker API",
+		"pt": "Token da API Maker",
+		"nl": "Maker API-token",
+		"fr": "Jeton d'API Maker",
+		"it": "Token API Maker",
+		"es": "Token de la API de Maker",
+		"pl": "Token Maker API",
+		"zh-cn": "Maker API 代币"
+	}
+};
